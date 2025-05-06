b3b9a8ca91c448fc8792d1f7904cdc8c0186fc52
Antsa46 <amuunoja@hotmail.com>
2025-05-06 21:46:58 +0300

    Fullscreen test

--- js/oliot/UIController.js
+++ js/oliot/UIController.js
@@ -5,11 +5,54 @@ export class UIController {
     this.peite = document.getElementById('peite');
     this.mietelauseet = mietelauseetInstance;
     this.nakyvissa = true;
+    this.lastTap = 0;
     this.init();
   }
 
   init() {
     document.addEventListener('mousemove', () => this.showUI());
+
+    document.addEventListener('dblclick', () => this.toggleFullscreen());
+
+    document.addEventListener('touchstart', (e) => this.detectDoubleTap(e));
+  }
+
+  detectDoubleTap(event) {
+    const currentTime = new Date().getTime();
+    const tapLength = currentTime - this.lastTap;
+
+    if (tapLength < 400 && tapLength > 0) {
+      this.toggleFullscreen();
+      event.preventDefault(); 
+    }
+
+    this.lastTap = currentTime;
+  }
+
+  toggleFullscreen() {
+    const elem = document.documentElement;
+
+    if (!document.fullscreenElement) {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+      } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
   }
 
   showUI() {
